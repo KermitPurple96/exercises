95b1abd4d9c0091ee4f7e2a9314ad64e5753d79a
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2025-02-08 17:11:30 +0100

    Update diamond.cpp

--- diamond.cpp
+++ diamond.cpp
@@ -13,12 +13,10 @@ int main()
     int mid = num / 2;
 
     for (int i = mid; i >= 0; i--) {
-        // 3 - 2 - 1 - 0
         for (int j = i; j >= 0; j--) {
             cout << " ";
         }
         ast = num - (i * 2);
-        // 1 - 3 - 5
         for (k = 1; k <= ast; k++) {
             cout << "*";
         }
@@ -26,14 +24,10 @@ int main()
     }
 
     for (int i = 0; i <= mid; i++) {
-        // 1 - 2 - 3
-
         for (int j = 0; j <= (i+1); j++) {
-            // 1 - 3 - 5 
             cout << " ";
         }
         ast = num - (i*2) - 2;
-        // 5 - 3 - 1
 
         for (k = (ast - 1); k >= 0; k--) {
             if (k == num) {
